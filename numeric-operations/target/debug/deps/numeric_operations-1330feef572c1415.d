36829ffd4f8951302c2e46bac1c597723398f4d7
Raven <ravend2013@gmail.com>
2025-06-17 00:07:00 +0100

    chore: remove target output

--- numeric-operations/target/debug/deps/numeric_operations-1330feef572c1415.d
+++
@@ -1,5 +0,0 @@
-/Users/raven/github/rust-book/numeric-operations/target/debug/deps/libnumeric_operations-1330feef572c1415.rmeta: src/main.rs
-
-/Users/raven/github/rust-book/numeric-operations/target/debug/deps/numeric_operations-1330feef572c1415.d: src/main.rs
-
-src/main.rs:
--- numeric-operations/target/debug/deps/numeric_operations-e5b466470b445bd0.d
+++
@@ -1,5 +0,0 @@
-/Users/raven/github/rust-book/numeric-operations/target/debug/deps/libnumeric_operations-e5b466470b445bd0.rmeta: src/main.rs
-
-/Users/raven/github/rust-book/numeric-operations/target/debug/deps/numeric_operations-e5b466470b445bd0.d: src/main.rs
-
-src/main.rs:
